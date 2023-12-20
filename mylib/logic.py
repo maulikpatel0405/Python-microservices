d64076675dd5fe772df0c3a2550365ffeb25ba15
Maulik <70161950+maulikpatel0405@users.noreply.github.com>
2023-12-20 04:45:47 +0000

    adding format step.

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,8 +1,9 @@
 import wikipedia
 
-def wiki(name="War Goddess", length = 1):
-    """ This is Wikipedia fetcher."""
 
-    my_wiki = wikipedia.summary(name,length)
+def wiki(name="War Goddess", length=1):
+    """This is Wikipedia fetcher."""
+
+    my_wiki = wikipedia.summary(name, length)
 
     return my_wiki
