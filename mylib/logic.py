ad7ef0bf25309d14d9571883bd4e37e72978bc88
Maulik <70161950+maulikpatel0405@users.noreply.github.com>
2023-12-20 05:45:53 +0000

    adding acli functinality.

--- mylib/logic.py
+++ mylib/logic.py
@@ -7,3 +7,10 @@ def wiki(name="War Goddess", length=1):
     my_wiki = wikipedia.summary(name, length)
 
     return my_wiki
+
+def search_wiki(name):
+    """Search wikipedia for names"""
+
+    results = wikipedia.search(name)
+
+    return results
